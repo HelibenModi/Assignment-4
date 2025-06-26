508fd3dee5180f69b3ed8efc6a117103ed401bc0
HelibenModi <helimodi710@gmail.com>
2025-06-26 20:13:20 +0000

    linked css

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,17 +1,19 @@
 <?php
-if (isset($_SESSION['auth']) == 1) {
+// Redirect if user is logged in
+if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
     header('Location: /reminders');
+    exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="icon" href="/favicon.png">
-    <title>COSC 4806</title>
+<head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
+    <title>COSC 4806</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.png">
+
+    <!-- ✅ Pointing correctly to public/style.css -->
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
